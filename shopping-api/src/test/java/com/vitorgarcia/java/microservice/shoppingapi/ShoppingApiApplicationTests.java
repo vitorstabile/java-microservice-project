b2e6c1218fcd41d2124ab696a9d2db991bb03221
vitorstabile <vitor.stabile@hotmail.com>
2024-02-29 20:58:22 +0000

    First Commit

---
+++ shopping-api/src/test/java/com/vitorgarcia/java/microservice/shoppingapi/ShoppingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorgarcia.java.microservice.shoppingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
