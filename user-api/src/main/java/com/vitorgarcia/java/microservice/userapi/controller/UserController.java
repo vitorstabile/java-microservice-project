579e228002f44725c706eece257fa70a8f563b1b
vitorstabile <vitor.stabile@hotmail.com>
2024-02-29 21:16:17 +0000

    Create UserDTO, and method to return a list of users as Json

---
+++ user-api/src/main/java/com/vitorgarcia/java/microservice/userapi/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.vitorgarcia.java.microservice.userapi.controller;
+
+import com.vitorgarcia.java.microservice.userapi.dto.UserDTO;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    public	static	List<UserDTO> users = new ArrayList<UserDTO>();
+
+    @PostConstruct
+    public	void	initiateList()	{
+        UserDTO userDTO = new UserDTO();
+        userDTO.setName("John Snow");
+        userDTO.setTaxId("111222333");
+        userDTO.setAddress("Lombard Street");
+        userDTO.setEmail("johnsnow@abc.com");
+        userDTO.setPhone("9191919191");
+        userDTO.setSubscriptionDate(new Date());
+
+        UserDTO userDTO2 = new UserDTO();
+        userDTO2.setName("Mike Creed");
+        userDTO2.setTaxId("444555666");
+        userDTO2.setAddress("Gold Street");
+        userDTO2.setEmail("mikecreed@abc.com");
+        userDTO2.setPhone("9292929292");
+        userDTO2.setSubscriptionDate(new Date());
+
+        UserDTO userDTO3 = new UserDTO();
+        userDTO3.setName("Luci Lee");
+        userDTO3.setTaxId("777888999");
+        userDTO3.setAddress("Green Street");
+        userDTO3.setEmail("lucilee@abc.com");
+        userDTO3.setPhone("9393939393");
+        userDTO3.setSubscriptionDate(new Date());
+
+        users.add(userDTO);
+        users.add(userDTO2);
+        users.add(userDTO3);
+    }
+
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping("/users")
+    public List<UserDTO> getUsers() {
+        return users;
+    }
+
+}
---
+++ user-api/src/main/java/com/vitorgarcia/java/microservice/userapi/dto/UserDTO.java
@@ -0,0 +1,61 @@
+package com.vitorgarcia.java.microservice.userapi.dto;
+
+import java.util.Date;
+
+public class UserDTO {
+
+    private	String	name;
+    private	String	taxId;
+    private	String	address;
+    private	String	email;
+    private	String	phone;
+    private Date subscriptionDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTaxId() {
+        return taxId;
+    }
+
+    public void setTaxId(String taxId) {
+        this.taxId = taxId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getSubscriptionDate() {
+        return subscriptionDate;
+    }
+
+    public void setSubscriptionDate(Date subscriptionDate) {
+        this.subscriptionDate = subscriptionDate;
+    }
+}
